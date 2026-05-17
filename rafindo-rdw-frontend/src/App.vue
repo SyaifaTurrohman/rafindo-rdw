f2b8527e38a9614d6c76a6c8bd0b8444614db0f7
Syaifa Turrohman <syaifaturrohman835@gmail.com>
2026-05-17 14:32:18 +0700

    commit

--- rafindo-rdw-frontend/src/App.vue
+++ rafindo-rdw-frontend/src/App.vue
@@ -130,7 +130,7 @@
 import { ref, reactive } from 'vue';
 
 // Konfigurasi URL API Python Backend
-const API_BASE_URL = 'https://rafindo-rdw.vercel.app/';
+const API_BASE_URL = 'https://rafindo-rdw.vercel.app/api';
 
 // State Autentikasi
 const isLoggedIn = ref(false);
